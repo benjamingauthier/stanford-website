169c3f1d4705ba12cba1a53464561cf62317d023
Benjamin Gauthier <benjamin-gauthier@live.fr>
2015-04-08 22:39:42 +0200

    Script subscription ok, test Session

--- script_end_subscription.php
+++ script_end_subscription.php
@@ -1,19 +1,22 @@
 <?php
 include('connectbdd.php');
-
+if(isset($_SESSION['ndd'])) {
+    $ndd = $_SESSION['ndd'];
+    unset($_SESSION['ndd']);
+}
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $address = $_POST['address'];
 $postcode = $_POST['postcode'];
 $city = $_POST['city'];
 
-$stmt = $dbh->prepare("UPDATE users SET firstname = :firstname AND lastname = :lastname AND address = :address AND postcode = :postcode AND city = :city WHERE id = :id");
+$stmt = $dbh->prepare("UPDATE users SET firstname = :firstname, lastname = :lastname, address = :address, postcode = :postcode, city = :city WHERE ndd = :ndd");
 
 $stmt->bindValue(':firstname', $firstname);
 $stmt->bindValue(':lastname', $lastname);
 $stmt->bindValue(':address', $address);
 $stmt->bindValue(':postcode', $postcode);
 $stmt->bindValue(':city', $city);
-$stmt->bindValue(':id', $dbh->lastInsertId());
+$stmt->bindValue(':ndd', $ndd);
 $stmt->execute();
 
--- script_subscription.php
+++ script_subscription.php
@@ -1,8 +1,9 @@
 <?php
 include('connectbdd.php');
+session_start();
 
 $ndd = $_POST['ndd'];
-
+$_SESSION['ndd'] = $ndd;
 $stmt = $dbh->prepare("INSERT INTO users (ndd) VALUES (:ndd)");
 
 $stmt->bindValue(':ndd', $ndd);
