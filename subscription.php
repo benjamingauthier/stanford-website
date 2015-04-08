290ae031c2dd409548f325e62a6d224673c55b94
Benjamin Gauthier <benjamin-gauthier@live.fr>
2015-04-09 01:08:23 +0200

    Subscription page ready for test

--- subscription.php
+++ subscription.php
@@ -3,10 +3,8 @@
     <!-- ============================================================= MAIN ============================================================= -->
 
     <main>
-
     <div class="container inner">
         <div class="row">
-
             <div class="col-md-8 inner-right inner-bottom-md">
 
                 <!-- ============================================================= SECTION – CONTACT FORM ============================================================= -->
@@ -17,25 +15,20 @@
                                 <h1>Faites comme des dizaines d'étudiants</h1>
                                 <p>Rejoignez Standio et mettez toutes les chances de votre coté pour votre recherche de stage ou d'emploi !</p><br><br>
                             </header>
-                        </div><!-- /.col -->
+                            <form id="contactform" class="forms" action="script_subscription.php" method="post">
 
-
-                    <form id="contactform" class="forms" action="script_subscription.php" method="post">
-
-                        <div class="row">
-                            <div class="col-xs-4">
-                                <span>www.
-                                <input type="text" name="ndd" id="ndd" class="form-control" placeholder="Site web" onkeyup="ndd_validation()">
-                                .fr</span>
-                                <i id="result" class="fa"></i>
-                            </div><!-- /.col -->
-                        </div><!-- /.row -->
-                        <button type="submit" class="btn btn-default btn-submit">Envoyer le message</button>
-
-                    </form>
-
-                    <div id="response"></div>
-
+                                <div class="row">
+                                    <div class="col-lg-6 col-centered">
+                                        <div class="input-group">
+                                            <input type="text" name="ndd" id="ndd" class="form-control" placeholder="Site web" onkeyup="ndd_validation()">
+                                            <span class="input-group-addon" id="basic-addon2">.fr</span>
+                                            &nbsp;<i id="result" class="fa fa-2x"></i>
+                                        </div>
+                                    </div><!-- /.col -->
+                                </div><!-- /.row -->
+                                <button type="submit" class="btn btn-default btn-submit" disabled>Inscription</button>
+                            </form>
+                        </div>
                 </section>
 
                 <!-- ============================================================= SECTION – CONTACT FORM : END ============================================================= -->
@@ -189,80 +182,29 @@
     <!-- ============================================================= MAIN : END ============================================================= -->
 
 <script language="JavaScript">
-    function createInstance()
-    {
-        var req = null;
-        if(window.XMLHttpRequest) {
-            req = new XMLHttpRequest();
-        }
-        else if (window.ActiveXObject) {
-            try {
-                req = new ActiveXObject("Msxml2.XMLHTTP");
-            } catch (e) {
-                try {
-                    req = new ActiveXObject("Microsoft.XMLHTTP");
-                } catch (e) {
-                    alert("XHR not created");
-                }
-            }
-        }
-        return req;
-    }
-
-    function storing(data)
-    {
-
-        if(data == 1) {
-            document.getElementById('registered').style.display = 'none';
-            document.getElementById('available').style.display = 'block';
-        }
-        else {
-            document.getElementById('available').style.display = 'none';
-            document.getElementById('registered').style.display = 'block';
-        }
-    }
-
     function ndd_validation(element)
     {
-       /* var req =  createInstance();
-        var ndd = document.getElementById('ndd').value;
-        var data = "ndd=" + ndd + ".fr";
-        req.onreadystatechange = function()
-        {
-            if(req.readyState == 4)
-            {
-                if(req.status == 200)
-                {
-                    storing(req.responseText);
-                }
-                else
-                {
-                    alert("Error: returned status code " + req.status + " " + req.statusText);
-                }
-            }
-        };
-
-        req.open("POST", "http://vps141243.ovh.net/dev/standio/bgauthie/example.php", true);
-        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        req.send(data);*/
         $.ajax({
-            url: 'http://www.one.com/test-domain.do',
+            url: 'https://standio.fr/checkdomain.php',
             type: 'GET',
             dataType: 'json',
             data: {
-                domain: $('#ndd').val()
+                domain: $('#ndd').val()+'.fr'
             },
             error: function() {
-                $("#result").removeClass('fa-check-circle').addClass("fa-times-circle")
+                $("#result").removeClass('fa-check-circle yellow').addClass("fa-times-circle black")
+                $("input[type=submit]").attr("disabled", "disabled")
             },
             success: function(data) {
                 if (data.available == true)
                 {
-                    $("#result").removeClass('fa-times-circle').addClass("fa-check-circle")
+                    $("#result").removeClass('fa-times-circle black').addClass("fa-check-circle yellow")
+                    $("input[type=submit]").removeAttr("disabled");
                 }
                 else
                 {
-                    $("#result").removeClass('fa-check-circle').addClass("fa-times-circle")
+                    $("#result").removeClass('fa-check-circle yellow').addClass("fa-times-circle black")
+                    $("input[type=submit]").attr("disabled", "disabled")
                 }
             }
         });
